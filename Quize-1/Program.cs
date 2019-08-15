b0edd5d5fad052e4820636c8113428cdb11a124e
abhi <abrahmbhatt03@gmai.com>
2019-08-15 13:57:24 -0500

    Add project files.

---
+++ Quize-1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quize_1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new int[] { 1,2, 3, 4, 5, 6};
+            var result = GetUniqueElement(array);
+            var resultOfDuplicate = IsContainDubplicateEelement(array);
+            var resultOfSet = UsingSetForDuplicate(array);
+            Console.WriteLine(resultOfSet);
+            Console.WriteLine(resultOfDuplicate);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+
+        public static bool IsContainDubplicateEelement(int[] arr)
+        {
+
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    if (i != j && arr[i] == arr[j])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        public static int GetUniqueElement(int[] arr)
+        {
+            //var array = arr;
+            //Console.WriteLine(arr[0]);
+
+            for(int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    if(i!=j && arr[i] == arr[j])
+                    {
+                        break;
+                    }
+                    if (j == arr.Length-1)
+                    {
+                        return arr[i];
+                    }
+                }
+            }
+            return 0;
+        }
+        
+        public static bool UsingSetForDuplicate(int[] arr)
+        {
+
+            HashSet<int> set = new HashSet<int>(arr);
+            if (arr.Length != set.Count())
+                return false;
+
+            return true;            
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Quize_1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        int[] array1 = new int[] { 1, 1, 2, 2, 3, 4, 4, 5 };
+        int[] array3 = new int[] { 1, 1, 2, 2,4,4};
+        int[] array2 = new int[] { 1, 2, 3, 4, 5, 6 };
+
+        [TestMethod]        
+        public void TestMethod1()
+        {
+            var result = Program.GetUniqueElement(array1);
+            Assert.AreEqual(3, result);
+            var result1 = Program.GetUniqueElement(array3);
+            Assert.AreEqual(0, result1);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var result = Program.IsContainDubplicateEelement(array1);
+            Assert.AreEqual(false, result);
+            var result1 = Program.IsContainDubplicateEelement(array2);
+            Assert.AreEqual(true, result1);
+        }
+    }
+}
